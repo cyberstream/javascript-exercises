be7c87d685457a11ef20b2a36e8b0e5b09ee83a7
Eli Mitchell <985041+cyberstream@users.noreply.github.com>
2022-03-12 21:08:32 -0900

    Solve sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (int1, int2) {
+  // find min and max; args can be entered in either order
+  const minInt = Math.min(int1, int2);
+  const maxInt = Math.max(int1, int2);
 
+  if (minInt < 0 || typeof int1 === NaN || typeof int2 === NaN) return 'ERROR';
+
+  let sum = 0;
+  let i = minInt;
+
+  // Sum all integers from minInt to maxInt
+  while (i <= maxInt) {
+    sum += i;
+    
+    i++;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
