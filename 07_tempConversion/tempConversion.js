74ae13fe5e3ec6717349ca843bbef0b010c6a83f
Eli Mitchell <985041+cyberstream@users.noreply.github.com>
2022-03-12 21:38:43 -0900

    Solve tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,26 @@
-const ftoc = function() {
+const ftoc = function(tempF) {
+  if (typeof tempF !== 'number') return 'ERROR';
 
+  // [°C] = ([°F] − 32) × 5⁄9
+  const tempC = (tempF - 32) * (5/9);
+
+  return roundOnePlace(tempC);
 };
 
-const ctof = function() {
+const ctof = function(tempC) {
+  if (typeof tempC !== 'number') return 'ERROR';
 
+  // [°F] = [°C] × 9⁄5 + 32
+  const tempF = (tempC * 9/5) + 32;
+
+  return roundOnePlace(tempF);
 };
+
+function roundOnePlace(num) {
+  return (Math.round(num * 10) / 10);
+}
+
+console.log(ftoc(32));
 
 // Do not edit below this line
 module.exports = {
